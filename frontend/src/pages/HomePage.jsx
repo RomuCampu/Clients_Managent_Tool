a1b2769fb7c46c7d679dab932b31c51f45f711cc
romucampu <romucampu@gmail.com>
2021-03-07 02:28:51 +0100

    And the homepage...

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { CarouselComponent } from '../components/CarouselComponent'
 
 export const HomePage = () => {
   return (
     <React.Fragment>
-      <h1 className="text-center mt-5">Welcome Screen</h1>
+      {/* <h1 className="text-center mt-5">Welcome Screen</h1> */}
+      {/*  */}
+      <CarouselComponent />
     </React.Fragment>
   )
 }
